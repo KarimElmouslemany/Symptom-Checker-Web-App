1410124dc2dd589200396a77419dd071c78f81e3
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-02-04 23:24:46 +0000

    created a js script that creates  dives form A to Z that contains symptoms for that corespondent letter.

--- interaction.js
+++ interaction.js
@@ -1,16 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-   // creating an array object 
-   const symptoms = [{
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // creating an array object
+  const symptoms = [
+    {
       symptoms_name: "fever",
-      symptoms_description: "run"
+      symptoms_description: "run",
+    },
+    {
+      symptoms_name: "cough",
+      symptoms_description: "freeze",
+    },
+  ];
+  const number_of_in_dives = document.getElementById("symptoms1"); //gets the element by with a certain value
+  const number_of_outer_dives = document.getElementById("outer_div"); //gets the element by with a certain value
+  const storage_outer_div = {}; // An object used as a lookup table to store outer divs by letter (A–Z)
+  let outer_dives; // defining a variable outer_dives
+  for (const letter of letters) {
+    // loops through all the letters and creates
+    outer_dives = document.createElement("div"); // create a div element
+    outer_dives.className = "outerdivs"; // assigns a class called outer
+    const heading = document.createElement("h2"); // creates a header tag and assigns it to heading variable.
+    heading.textContent = letter; // adds the letters as a heading.
+    outer_dives.append(heading); // added the heading tag to the div outer_dives
+    number_of_in_dives.append(outer_dives); // put the outerdivs to the page
+    storage_outer_div[letter] = outer_dives; // stores the div for later
+  }
 
-   }];
-   const number_of_dives = document.getElementById("symptoms1");
-
-   for(let i =0; i< 20; i++){
-      const dives =  document.createElement("div"); // create a div element 
-      dives.className = "symptoms"; // gives the dives a class called symptoms
-      dives.innerHTML = `<h2>${symptoms[0].symptoms_name}</h2> <p>${symptoms[0].symptoms_description}<p>`; 
-      number_of_dives.append(dives); // adds the dives to the dummy div element called symptoms1
-   }
+  for (let i = 0; i < symptoms.length; i++) {
+    // loops through the object array
+    const inner_div = document.createElement("div"); // creating outer div
+    inner_div.className = "symptoms"; // gives the dives a class called symptoms
+    inner_div.innerHTML = `<h2>${symptoms[i].symptoms_name}</h2> <p>${symptoms[i].symptoms_description}<p>`; // adds the symptoms name and the description
+    const letter = symptoms[i].symptoms_name[0].toUpperCase();
+    storage_outer_div[letter].append(inner_div); // looks up the correct outer dive and adds the symptoms and description to it.
+  }
 });
