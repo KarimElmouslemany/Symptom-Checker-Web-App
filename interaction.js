d31d8b6ee66e4a9b9d14d858343013a93fa921d0
karim elmouslemany <karimelmouslemany@gmail.com>
2026-03-06 11:59:32 +0000

    add detials and summary and also  addthe description of each symptome from the api.

--- interaction.js
+++ interaction.js
@@ -1,13 +1,14 @@
-document.addEventListener("DOMContentLoaded", async() => {
+document.addEventListener("DOMContentLoaded", async () => {
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   // creating an array object
   const API_KEY = "Rkjfe4Pk6TpvJuvF1TyZgjAJh3BZJp4t"; // API Key
-  const BASE_URL = "https://int.api.service.nhs.uk/nhs-website-content/symptoms"; // url for where we are getting everything from 
+  const BASE_URL =
+    "https://int.api.service.nhs.uk/nhs-website-content/symptoms"; // url for where we are getting everything from
   const number_of_in_dives = document.getElementById("symptoms1"); //gets the element by with a certain value
   const storage_outer_div = {}; // An object used as a lookup table to store outer divs by letter (A–Z)
   for (const letter of letters) {
     // loops through all the letters and creates
-   const outer_dives = document.createElement("div"); // create a div element
+    const outer_dives = document.createElement("div"); // create a div element
     outer_dives.className =
       "rounded-2xl border border-slate-200 bg-white p-4 shadow-sm"; // creates styling for the borders of the dives
     const heading = document.createElement("h2"); // creates a header tag and assigns it to heading variable.
@@ -16,22 +17,26 @@ document.addEventListener("DOMContentLoaded", async() => {
     number_of_in_dives.append(outer_dives); // put the outerdivs to the page
     storage_outer_div[letter] = outer_dives; // stores the div for later
   }
-  for(const letter of letters){
-    const response = await fetch(`https://corsproxy.io/?https://int.api.service.nhs.uk/nhs-website-content/symptoms/?category=${letter}`,{headers:{"apikey":API_KEY}});
+  for (const letter of letters) {
+    const response = await fetch(
+      `https://corsproxy.io/?https://int.api.service.nhs.uk/nhs-website-content/symptoms/?category=${letter}`,
+      { headers: { apikey: API_KEY } },
+    );
     const data = await response.json();
-    if(!data.significantLink){
+    if (!data.significantLink) {
       continue;
     }
     const NHS_symptoms = data.significantLink;
-    
-    console.log(data.significantLink)
-    for(const symptom of NHS_symptoms ){
+    console.log(data);
+    for (const symptom of NHS_symptoms) {
       const inner_div = document.createElement("div"); // creating an inner div
       inner_div.textContent = symptom.name;
       storage_outer_div[letter].append(inner_div);
+      inner_div.innerHTML = `<details>
+      <summary>${symptom.name}</summary>
+      <p>${symptom.description}</p>
+      </details>`
     }
-     await new Promise(resolve => setTimeout(resolve, 2000)); // waits a sold 1 seconds before it sends a api request for every letter. 
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // waits a sold 2 seconds before it sends a api request for every letter.
   }
-  
-
 });
