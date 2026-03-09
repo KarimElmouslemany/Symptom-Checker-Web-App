ea1d5178dff01576f271dc623205e20f4e0491f5
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-03-09 22:09:13 +0000

    the server file reads the symptom.json and sends the content back to the frontend which is being listend on port 3000.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express(); // creates a server
+app.use(cors()); // turns on CORS( this is for nhs api to work)
+
+// load the symptoms data from the JSON file
+const symptoms = JSON.parse(fs.readFileSync("symptoms.json")); // reads the symptom data from the symptom.json file and converts it into a Javascript object
+
+// NHS symptoms route(get request)
+app.get("/symptoms", (req, res) => {
+  // when the front end calls this section grabs a letter and looks it up in the  symptoms object and send the array of that letter. and if nothing has been found itt send and empty array
+  const category = req.query.category.toUpperCase();
+  res.json(symptoms[category] || []);
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000")); // starts server at port 3000
