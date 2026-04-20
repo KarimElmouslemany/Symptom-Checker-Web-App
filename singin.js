e5d9edf00a834231e3a9dffbdc82f3b97793e353
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-04-20 18:29:56 +0100

    made it where it can handle errors

--- singin.js
+++ singin.js
@@ -1,31 +1,87 @@
 import { auth } from "./Firebase.js";
-import {
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
 const form = document.getElementById("login_form");
 form.addEventListener("submit", async (e) => {
+  let verification = null;
   e.preventDefault();
-  const username = document.getElementById("login_username_input").value.trim();
+  const username = document.getElementById("login_username_input").value;
+  const username_for_validation = document.getElementById("login_username_input");
   const password = document.getElementById("login_password_input").value;
+  const error_message = document.getElementById("error_display");
   try {
-    await signInWithEmailAndPassword(auth, username, password);
-    alert("valid user");
-    window.location.href = "main.html";
+    verification = handling_verification(username,password,error_message,username_for_validation);
+    console.log("verification is ", verification);
+    if (verification == true) {
+      await signInWithEmailAndPassword(auth, username, password);
+      window.location.href = "main.html";
+    } else {
+      console.log("the error came from handling_verification function");
+    }
   } catch (err) {
-    if (err.code === "auth/invalid-credential")
-      alert("Email or password is wrong");
-    else if (err.code === "auth/user-not-found")
-      alert("No account found for that email");
-    alert(err.message);
-  }
-});
+    error_handling(err, error_message);
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("Logged in as:", user.email);
-  } else {
-    console.log("Not logged in");
+    
   }
 });
+// checker users passwords length
+// check if users email has an @ symbols and gmail,yahoo and stuff
+// display errors correctly to users
+function handling_verification(
+  username,
+  password,
+  error_message,
+  EmailForValidation
+ 
+) {
+  let verification_in_function = false;
+  error_message.classList.remove("hidden");
+  if (username == null || username == "" || username.length == 0) {
+    console.log("username filed is empty please enter the username ");
+    error_message.textContent =
+      "username filed is empty please enter the username";
+    return (verification_in_function = false);
+  }
+  if (password == null || password == "") {
+    console.log("password filed is empty please enter a valid password");
+    error_message.textContent =
+      "password filed is empty please enter a valid password";
+    return (verification_in_function = false);
+  }
+  if (password.length < 6) {
+    console.log("password  does not match");
+    error_message.textContent = "password  does not match";
+    return (verification_in_function = false);
+  }
+  if (EmailForValidation.validity.typeMismatch) {
+    console.log("This is not a valid email. Please enter a valid email");
+    error_message.textContent =
+      "This is not a valid email. Please enter a valid email";
+    return (verification_in_function = false);
+  }
+  
+  else {
+    console.log("nothing found wrong everything is good");
+    error_message.classList.add("hidden");
+    return (verification_in_function = true);
+  }
+}
+
+function error_handling(error, error_message) {
+  error_message.classList.remove("hidden");
+  if (error.code === "auth/invalid-credential") {
+    error_message.textContent = "Email or password is wrong";
+  } else if (error.code === "auth/too-many-requests") {
+    error_message.textContent = "Too many failed attempts. Please try again later";
+  } else if (error.code === "auth/network-request-failed") {
+    error_message.textContent = "Network error. Please check your connection";
+  }else{
+    error_messages.textContent = "something went wrong. Please try again";
+  }
+ 
+  console.log("━━━━━━━━━━━━━━━━━━━━━━");
+  console.log("Error code:", error.code);
+  console.log("Error message:", error.message);
+  console.log("Full error object:", error);
+  console.log("━━━━━━━━━━━━━━━━━━━━━━");
+}
--- singup.js
+++ singup.js
@@ -5,16 +5,97 @@ const form = document.getElementById("signup_form");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-
+  let verification = false;
   const email = document.getElementById("signup_email_input").value.trim();
   const password = document.getElementById("signup_password_input").value;
-
+  const EmailForValidation = document.getElementById("signup_email_input");
+  const error_message = document.getElementById("error_display");
+  const confirm_password = document.getElementById(
+    "signup_password_confirm_input",
+  ).value;
   try {
-    await createUserWithEmailAndPassword(auth, email, password);
-    alert("Account created");
+    verification = handling_verification(
+      email,
+      password,
+      error_message,
+      EmailForValidation,
+      confirm_password,
+    );
+    if (verification == true) {
+      await createUserWithEmailAndPassword(auth, email, password);
+      alert("Account created");
       window.location.href = "login.html";
+    }
   } catch (err) {
-  console.error("Firebase signup error:", err);
-  alert(`${err.code ?? ""} ${err.message ?? err}`);
-}
+    // console.error("Firebase signup error:", err);
+    error_handling(err, error_message);
+  }
 });
+
+function handling_verification(
+  username,
+  password,
+  error_message,
+  EmailForValidation,
+  confirm_password,
+) {
+  let verification_in_function = false;
+  error_message.classList.remove("hidden");
+  if (username == null || username == "" || username.length == 0) {
+    console.log("Email filed is empty please enter the username ");
+    error_message.textContent =
+      "Email filed is empty please enter the username";
+    return (verification_in_function = false);
+  }
+  if (password == null || password == "") {
+    console.log("password filed is empty please enter a valid password");
+    error_message.textContent =
+      "password filed is empty please enter a valid password";
+    return (verification_in_function = false);
+  }
+  if (password.length < 6) {
+    console.log("password length is less than 6 characters");
+    error_message.textContent = "password length is less than 6 characters";
+    return (verification_in_function = false);
+  }
+  if (EmailForValidation.validity.typeMismatch) {
+    console.log("This is not a valid email. Please enter a valid email");
+    error_message.textContent =
+      "This is not a valid email. Please enter a valid email";
+    return (verification_in_function = false);
+  }
+  if (!username.includes("@") || !username.includes(".")) {
+    console.log("This is not a valid email. Please enter a valid email");
+    error_message.textContent =
+      "This is not a valid email. Please enter a valid email";
+    return (verification_in_function = false);
+  }
+  if (confirm_password != password) {
+    console.log("password input does not match confirm password input ");
+    error_message.textContent = "password does not match";
+    return (verification_in_function = false);
+  } else {
+    console.log("nothing found wrong everything is good");
+    error_message.classList.add("hidden");
+    return (verification_in_function = true);
+  }
+}
+function error_handling(error, error_message) {
+  error_message.classList.remove("hidden");
+  if (error.code == "auth/email-already-in-use") {
+    error_message.textContent =
+      "An account with this email already exists. Please log in instead.";
+  }
+  if (error.code == "auth/too-many-requests") {
+    error_message.textContent = "Too many attempts. Please try again later";
+  }
+  if (error.code == "auth/network-request-failed") {
+    error_message.textContent = "Network error. Please check your connection";
+  } else {
+    console.log("━━━━━━━━━━━━━━━━━━━━━━");
+    console.log("Error code:", error.code);
+    console.log("Error message:", error.message);
+    console.log("Full error object:", error);
+    console.log("━━━━━━━━━━━━━━━━━━━━━━");
+  }
+}
