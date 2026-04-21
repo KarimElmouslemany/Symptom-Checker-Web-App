411ca0a61f7322d389ca9900cc6124fce43a2ff0
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-04-21 23:51:20 +0100

    checks if someone has singed in and if not redricets the user to the singin page

---
+++ singout.js
@@ -0,0 +1,27 @@
+import { auth } from "./Firebase.js"; // import from the firebase.js file
+import { signOut} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js"; // importing the firebase signout function
+import {onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js"; // importing the firebase onAuthStateChanged function
+document.addEventListener("DOMContentLoaded", () => {
+  const signOut_button = document.getElementsByClassName("singout_button") //gets the class element 
+  for(let i = 0; i<signOut_button.length; i++){
+        signOut_button[i].addEventListener("click", signout);
+  }
+
+});
+
+// checks if the user is logged in.
+onAuthStateChanged(auth, (user) => {
+  if (user === null) {
+    // if user is null (so no one has logged in )
+    window.location.href = "login.html"; // goes to the login page
+  }
+});
+async function signout() {
+  try {
+    await signOut(auth); // sing out users ( does: Ends the user session ,Clears authentication state ,Triggers onAuthStateChanged Sets user = null).
+    window.location.href = "login.html"; // sends user to login page
+  } catch (err) {
+    console.error("Sign out failed:", err);
+    alert("Something went wrong signing out. Please try again.");
+  }
+}
