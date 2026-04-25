9856fbe64c3d09f58c48ac15cafc20b08dbdfec4
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-04-26 00:13:02 +0100

    dont need corse as i not really need to accepet request from other websites

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const cors = require("cors");
 const app = express(); // creates a server
-app.use(cors()); // turns on CORS( this is for nhs api to work)
 app.use(express.json()); // lets the server read json body that ai.js sends.
 const chatbot = require("./AI_server"); // import the ai_server file
 app.use(chatbot); // puts the file into the main server.
