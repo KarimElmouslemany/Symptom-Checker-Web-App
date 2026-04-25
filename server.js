fd89e2d51239a66272d1a92c52ccc8fe12d97e5a
Karim Elmouslemany <102916639+KarimElmouslemany@users.noreply.github.com>
2026-04-25 19:40:26 +0100

    deployment fix

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ app.use(cors()); // turns on CORS( this is for nhs api to work)
 app.use(express.json()); // lets the server read json body that ai.js sends.
 const chatbot = require("./AI_server"); // import the ai_server file
 app.use(chatbot); // puts the file into the main server.
-app.use(express.static('.'));
+// serve all HTML, CSS, JS and image files from this project folder
+app.use(express.static(__dirname));
 app.listen(process.env.PORT || 3000, () => console.log("Server running")); // starts server at the host server or port  3000 if host server not available
